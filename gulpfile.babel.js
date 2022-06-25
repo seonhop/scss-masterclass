6ca91d184e46e6464b65d385d4612571097476f1
seonhop <78877741+seonhop@users.noreply.github.com>
2022-06-24 21:22:28 -0700

    #3.2 Variables and Nesting
    #Variable
    SCSS 파일을 만들때 _을 넣는건 css로 변환되지 않길 원하는 것들을 저장함
    ㄴ $을 붙여서 사용
    
    #Nesting(둥지)
    부모 내 자식 요소들의 CSS를 굳이 class를 주지 않아도 된다.
    ㄴ매우 큰 장점이라고 생각함
    
    그런 active, hover 같은 활동적인 CSS를 넣어주고 싶다!?
    ㄴ& 넣기

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,34 +1,33 @@
 import gulp from "gulp";
 import del from "del";
-import sass from "gulp-sass";
 import minify from "gulp-csso";
 import autoprefixer from "gulp-autoprefixer";
 
-sass.compiler = require("node-sass");
+const sass = require("gulp-sass")(require("node-sass"));
 
 const routes = {
-  css: {
-    watch: "src/scss/*",
-    src: "src/scss/styles.scss",
-    dest: "dist/css"
-  }
+    css: {
+        watch: "src/scss/*",
+        src: "src/scss/styles.scss",
+        dest: "dist/css",
+    },
 };
 
 const styles = () =>
-  gulp
-    .src(routes.css.src)
-    .pipe(sass().on("error", sass.logError))
-    .pipe(
-      autoprefixer({
-        flexbox: true,
-        grid: "autoplace"
-      })
-    )
-    .pipe(minify())
-    .pipe(gulp.dest(routes.css.dest));
+    gulp
+        .src(routes.css.src)
+        .pipe(sass().on("error", sass.logError))
+        .pipe(
+            autoprefixer({
+                flexbox: true,
+                grid: "autoplace",
+            })
+        )
+        .pipe(minify())
+        .pipe(gulp.dest(routes.css.dest));
 
 const watch = () => {
-  gulp.watch(routes.css.watch, styles);
+    gulp.watch(routes.css.watch, { usePolling: true }, styles);
 };
 
 const clean = () => del(["dist/styles.css"]);
